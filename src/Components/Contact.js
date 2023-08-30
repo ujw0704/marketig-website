1d7ccc172d6bfd6fd8a080d22092f258b1b9c809
ujw0704 <ujwalsharma571@gmail.com>
2023-08-30 23:03:27 +0530

    added contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Contact() {
+
+  let[name ,setName]= useState()
+  let[email,setEmail]=useState()
+  let[subject,setSubject]=useState()
+  let[message,setMessage]=useState()
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Form submitted:', { name, email, subject, message });
+
+    
+    setName('');
+    setEmail('');
+    setSubject('');
+    setMessage('');
+};
   return (
-    <div>
-      
+    <div className='conatiner'>
+      <div className='form'>
+       <from onSubmit={handelsubmit} methods='post'>
+
+      <input className='name' type='text'name='uname'placeholder='Enter your name' value ={name}onClick={(e=>setName(e.target.value))}>Name</input>
+      <input  className ="email"type='text'name='uname'placeholder='Enter your email' value={email}onClick={(e=>setEmail(e.target.value))}>Email</input>
+      <input  className ="subject"type='text'name='uname'placeholder='Enter your subject'value={subject} onClick={(e=>setSubject(e.target.value))}>subject</input>
+      <textarea className='massage'placeholder='Enter your massage' value={message}onClick={(e=>setMessage(e.target.value))}></textarea>
+      <div className='btn'>
+
+                <button type='submit'>Submit</button>
+                </div>
+       </from>
     </div>
+      </div>
   )
 }
 
